95511b9342e3cdbfccb8382397f52eab7ae7ef00
MezianeKhalil <khalilmez2000@gmail.com>
2020-12-13 16:07:36 +0100

    fixig body

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html lang="ar">
         <Head />
-        <body>
+        <body className="dark:bg-blackTheme">
           <Main />
           <NextScript />
         </body>
